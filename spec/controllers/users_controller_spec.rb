5b591633acc128f26d2deee2d187759352708958
Nick Pisciotta <npisciotta@gmail.com>
2016-10-17 22:36:57 -0600

    Add user show endpoint and tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -8,4 +8,14 @@ RSpec.describe UsersController, type: :controller do
       expect(response).to have_http_status(:ok)
     end
   end
-end 
+
+  describe '#show' do
+    it 'responds with a 200' do
+      user_one = create_first_user
+
+      get :show, id: user_one.id
+
+      expect(response).to have_http_status(:ok)
+    end
+  end
+end
--- spec/requests/user_spec.rb
+++ spec/requests/user_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe "Retrieve all users" do
   describe "GET #index" do
     it "returns all users" do
-      user_one =  create_first_user                        email: "john@example.com", social_security_number: "111111111")
+      user_one =  create_first_user
       user_two =  create_second_user
 
       get "/users"
@@ -25,10 +25,23 @@ describe "Retrieve all users" do
   end
 end
 
-# describe "Retrieve a specfic user" do
-#   describe "GET #show" do
-#     it "returns one user" do
-#
-#     end
-#   end
-# end
+describe "Retrieve a specfic user" do
+  describe "GET #show" do
+    it "returns one user" do
+      user_one = create_first_user
+      user_two = create_second_user
+
+      get "/users/#{user_one.id}"
+
+      expect(response.status).to eq(200)
+
+      parsed_response = JSON.parse(response.body)
+
+      expect(parsed_response["id"]).to eq(user_one.id)
+      expect(parsed_response["first_name"]).to eq(user_one.first_name)
+      expect(parsed_response["last_name"]).to eq(user_one.last_name)
+      expect(parsed_response["email"]).to eq(user_one.email)
+      expect(parsed_response["social_security_number"]).to eq(user_one.social_security_number)
+    end
+  end
+end
