e31b8f4ff7d595e728a2c1095dc60fbb1f17d1f2
Nick Pisciotta <npisciotta@gmail.com>
2016-10-17 19:58:04 -0600

    Create user validations and test them

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+
+  it { should validate_presence_of(:first_name) }
+  it { should validate_presence_of(:last_name) }
+  it { should validate_presence_of(:email) }
+  it { should validate_presence_of(:social_security_number) }
+  it { should validate_length_of(:social_security_number).is_equal_to(9) }
+  it { should allow_value("example@email.com").for(:email) }
+  it { should_not allow_value("exampleemail.com").for(:email) }
+  
+end
